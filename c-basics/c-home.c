8658ca9753cf069a082649e2c8d7531c1b2bd7e1
José Miguel López Zuluaga <38506793+jmlopezzu@users.noreply.github.com>
2024-05-03 18:43:11 -0500

    first commit

---
+++ c-basics/c-home.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+    printf("Im learning C.\n");
+    printf("practicing tab \t");
+    printf("SWIM, BIKE & RUN \\");
+    printf("They call me \"Fisher\".");
+    return 0;
+}
